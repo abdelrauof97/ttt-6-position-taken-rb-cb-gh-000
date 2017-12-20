6f3274152649369a4166531ff15a923b9a7659d0
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-20 03:54:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,5 @@
 # code your #position_taken? method here!
-def disply_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+
 board = ["X", "", "", "", "", "", "", "", ""]
 
 def position_taken?(array, index)
