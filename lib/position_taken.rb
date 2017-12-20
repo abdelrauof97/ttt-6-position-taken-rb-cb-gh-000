d134743c951920075eae69149d4a5d57842aafe1
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-20 04:08:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,8 @@ board = ["", "", "", "", "", "", "", "", ""]
 
 def position_taken?(array, index)
   if "#{array[inex]}" == nil || "#{array[index]}" == "" || "#{array[index]}" == " "
-    nil == false
+    nil = false
   else
-    nil == true
+    nil = true
   end
 end
-position_taken?(board, 1)
-puts position_taken?(board, 1)
